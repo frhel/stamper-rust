11e4f3fd9cf2eadc439a27f097d6bd3e1617c18a
Freyr H <freyr.helga@gmail.com>
2023-02-07 13:45:48 +0000

    Initial commit Make file structure for data layer

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+mod models;
+use crate::models::{SongModel, SessionModel, DbModel};
+
+
+
+fn main() {
+    let song_model = SongModel {};
+    let session_model = SessionModel {};
+    let db_model = DbModel {};
+}
---
+++ src/models/mod.rs
@@ -0,0 +1,7 @@
+mod song_model;
+mod session_model;
+mod db_model;
+
+pub use song_model::SongModel;
+pub use session_model::SessionModel;
+pub use db_model::DbModel;
